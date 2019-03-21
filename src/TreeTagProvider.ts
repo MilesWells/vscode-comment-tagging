2b074e51adb7c64cca75e2d4a20e1593067ce419
Miles Wells <miles.wells@validic.com>
2019-03-21 11:58:19 -0400

    it works! needs configuration and probably improve performance

--- src/TreeTagProvider.ts
+++ src/TreeTagProvider.ts
@@ -1,45 +1,82 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import { promisify } from 'util';
+import { groupBy } from 'lodash';
 
-export class Node extends vscode.TreeItem {
-  lineNumber: number;
-  path: string;
-  tag: string;
-  description: string;
+export enum NodeType {
+  TAG_GROUP,
+  FILE_GROUP,
+  TAG
+}
+
+interface INode {
   children?: Node[];
+  description?: string;
+  fileName?: string;
+  label?: string;
+  path?: string;
+  tag: string;
+  type: NodeType;
+}
 
-  constructor(
-    {
-      tag,
-      description,
-      path,
-      lineNumber
-    }: {
-      lineNumber: number;
-      path: string;
-      tag: string;
-      description: string;
-    },
-    children?: Node[]
-  ) {
-    super(tag);
-    this.lineNumber = lineNumber;
+export class Node extends vscode.TreeItem implements INode {
+  children?: Node[];
+  description?: string;
+  fileName?: string;
+  path?: string;
+  tag: string;
+  type: NodeType;
+
+  constructor({
+    description,
+    fileName,
+    label,
+    path,
+    tag,
+    type,
+    children
+  }: INode) {
+    let collapse;
+    switch (type) {
+      case NodeType.FILE_GROUP:
+      case NodeType.TAG_GROUP:
+        collapse = vscode.TreeItemCollapsibleState.Collapsed;
+        break;
+      case NodeType.TAG:
+      default:
+        collapse = vscode.TreeItemCollapsibleState.None;
+        break;
+    }
+
+    super(tag, collapse);
+    this.children = children && children.length ? children : [];
+    this.description = description;
+    this.fileName = fileName;
+    this.label = label;
     this.path = path;
     this.tag = tag;
-    this.description = description;
-    this.children = children && children.length ? children : [];
+    this.type = type;
   }
 }
 
-export default class TreeTagProvider implements vscode.TreeDataProvider<Node> {
-  private _onDidChangeTreeData: vscode.EventEmitter<
-    any
-  > = new vscode.EventEmitter<any>();
-  readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData
-    .event;
+const readFile = promisify(fs.readFile);
 
-  // TODO: figure out what this does
-  public refresh() {
-    this._onDidChangeTreeData.fire();
+// unicode consortium considers this as the proper cross-platform regex for finding new lines
+const NEW_LINE_REGEX = /\r\n|[\n\v\f\r\x85\u2028\u2029]/;
+
+// @TODO make this configurable
+const TAG_REGEX = /\/\/[ ]?@(\S+) (.*$)/;
+
+export default class TreeTagProvider implements vscode.TreeDataProvider<Node> {
+  private _onDidChangeTreeData: vscode.EventEmitter<any>;
+  readonly onDidChangeTreeData: vscode.Event<any>;
+  private tree: Node[] = [];
+
+  constructor() {
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+    this.refresh();
   }
 
   public getTreeItem(element: Node) {
@@ -47,6 +84,101 @@ export default class TreeTagProvider implements vscode.TreeDataProvider<Node> {
   }
 
   public getChildren(element?: Node) {
-    return element && element.children ? element.children : [];
+    return element && element.children ? element.children : this.tree;
   }
+
+  public refresh = async () => {
+    // @TODO config for excluded globs
+    const files = await vscode.workspace.findFiles(
+      '**/*',
+      '**/node_modules/**'
+    );
+    const promises: Promise<Node[]>[] = [];
+    files.forEach(({ fsPath }) => {
+      promises.push(this.findTagsInFile(fsPath));
+    });
+
+    const results = await Promise.all(promises);
+
+    // flatten tags into one dimensional array
+    const flattenedResults = results
+      .filter(result => result.length > 0)
+      .reduce((acc, cur) => acc.concat(cur), []);
+
+    const groupedResults = groupBy(flattenedResults, 'tag');
+    const tags = Object.keys(groupedResults);
+
+    const tagGroupNodes: Node[] = [];
+
+    // for each tag, create a tag group node with the children being the matching tags
+    tags.forEach(tag => {
+      tagGroupNodes.push(
+        new Node({
+          tag,
+          label: tag,
+          type: NodeType.TAG_GROUP,
+          children: groupedResults[tag]
+        })
+      );
+    });
+
+    // group the children of each tag group by file path
+    const tree: Node[] = tagGroupNodes.map(tagGroup => {
+      const fileGroupNodes: Node[] = [];
+      const grouped = groupBy(tagGroup.children, 'path');
+      const paths = Object.keys(grouped);
+
+      paths.forEach(path => {
+        const label = grouped[path][0].fileName || 'UNKNOWN';
+        fileGroupNodes.push(
+          new Node({
+            children: grouped[path],
+            label,
+            tag: label,
+            type: NodeType.FILE_GROUP
+          })
+        );
+      });
+
+      return {
+        ...tagGroup,
+        children: fileGroupNodes
+      };
+    });
+
+    this.tree = tree.sort((a, b) => `${a.label}`.localeCompare(`${b.label}`));
+    this._onDidChangeTreeData.fire();
+  };
+
+  // finds all the tags in a given file
+  private findTagsInFile = async (path: string): Promise<Node[]> => {
+    const fileName = path.replace(/^.*[\\\/]/, '');
+    const text = (await readFile(path)).toString();
+    const lines = text.split(NEW_LINE_REGEX);
+    return lines
+      .map(
+        (line, idx): Node | null => {
+          const match = line.match(TAG_REGEX);
+          if (!match) {
+            return null;
+          }
+
+          const [_, tag, description] = match;
+
+          return {
+            fileName,
+            path,
+            tag,
+            type: NodeType.TAG,
+            label: `${idx + 1}: ${description}`,
+            command: {
+              title: '',
+              command: 'ct-tree.onClickTag',
+              arguments: [path, idx]
+            }
+          };
+        }
+      )
+      .filter((match): match is Node => match !== null); // type guard for stripping nulls
+  };
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,80 +1,36 @@
-// TODO: JSDOC
-
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import { promisify } from 'util';
 
-import { groupBy } from 'lodash';
+import TreeTagProvider from './TreeTagProvider';
 
-import TreeTagProvider, { Node } from './TreeTagProvider';
-
-const TREE_VIEW_ID = 'ct-tree-view';
-const VIEW_CONTAINER_ID = 'ct-view-container';
-const VIEW_ID = 'ct-view';
-
-const readFile = promisify(fs.readFile);
-
-// unicode consortium considers this as the proper cross-platform regex for finding new lines
-const NEW_LINE_REGEX = /\r\n|[\n\v\f\r\x85\u2028\u2029]/;
-
-// TODO: use capture groups. 1 is the tag name, 2 is the tag description
-const TAG_REGEX = /\/\/ @(\S+) (.*$)/;
+export const TREE_VIEW_ID = 'ct-tree-view';
+export const VIEW_CONTAINER_ID = 'ct-view-container';
+export const VIEW_ID = 'ct-view';
 
 export function activate(context: vscode.ExtensionContext) {
-  // SEE SOURCE FOR HELP: https://github.com/Gruntfuggly/todo-tree
+  // register provider
   const treeDataProvider = new TreeTagProvider();
+  vscode.window.registerTreeDataProvider(VIEW_ID, treeDataProvider);
 
-  // TODO: no idea if this works
-  vscode.commands.registerCommand('commentTagTree.refresh', () =>
-    treeDataProvider.refresh()
+  // refresh data on document save
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument(treeDataProvider.refresh)
   );
-  const todoTreeViewExplorer = vscode.window.createTreeView(VIEW_ID, {
-    treeDataProvider
-  });
 
-  context.subscriptions.push(todoTreeViewExplorer); // will get disposed when extension is deactivated
-
-  buildData();
-}
-
-// builds data for tree provider thing
-async function buildData() {
-  const files = await vscode.workspace.findFiles('**/*', '**/node_modules/**');
-  const promises: Promise<(Node | null)[]>[] = [];
-  files.forEach(({ fsPath }) => {
-    promises.push(findTagsInFile(fsPath));
-  });
-
-  const results = await Promise.all(promises);
-  const flattenedResults = results
-    .filter(result => result.length > 0)
-    .reduce((acc, curr) => acc.concat(curr), []);
-  const groupedResults = groupBy(flattenedResults, 'tag');
-  const tags = Object.keys(groupedResults).sort((a, b) => a.localeCompare(b));
-  console.log(tags);
-}
-
-// finds all the tags in a given file
-async function findTagsInFile(path: string) {
-  const text = (await readFile(path)).toString();
-  const lines = text.split(NEW_LINE_REGEX);
-  return lines
-    .map(
-      (line, idx): Node | null => {
-        const match = line.match(TAG_REGEX);
-        if (!match) {
-          return null;
-        }
-
-        const [_, tag, description] = match;
-
-        return {
-          lineNumber: idx + 1,
-          path,
-          tag,
-          description
-        };
+  // when we click a tag, show the document and put the cursor on the correct line
+  vscode.commands.registerCommand(
+    'ct-tree.onClickTag',
+    async (path, lineNumber) => {
+      await vscode.window.showTextDocument(vscode.Uri.parse(`file://${path}`));
+      if (!vscode.window.activeTextEditor) {
+        return;
       }
-    )
-    .filter(match => match !== null);
+
+      vscode.window.activeTextEditor.selection = new vscode.Selection(
+        lineNumber,
+        0,
+        lineNumber,
+        0
+      );
+    }
+  );
 }
